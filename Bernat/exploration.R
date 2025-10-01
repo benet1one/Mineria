c575ad7960313014de2e8723efd5ad7ab6a8e00d
benetsugarboy <benet1one@gmail.com>
2025-10-01 21:33:03 +0200

    started exploration

---
+++ Bernat/exploration.R
@@ -0,0 +1,7 @@
+
+# Song Popularity ----------------------------------------
+
+# We could try different transformations for the prediction, 
+# since the distribution is hard to explain.
+hist(songs$song_popularity)
+# One option is to make it uniform
