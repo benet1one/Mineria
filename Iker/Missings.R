df18bef1672743514e62b93dfb601c123f1dcb51
Torponcito <ikermaria.estudios@gmail.com>
2025-10-08 19:58:49 +0200

    IV
    Pre-missing info

--- Iker/Missings.R
+++ Iker/Missings.R
@@ -1,15 +1,36 @@
 library(dplyr, warn.conflicts = FALSE)
+library(tidyverse)
+" EXecute after reading.R code"
+"(idea for other part) Data integration is not rellevant in this project since there is only a single data source."
 
-# First we need to find all the possible missings formats in the data. If everything it's make correctly, 
-# in reading.R it has been standarized de missings.
-
+"Once the data has been integrated, we can summarize the characteristics of attributes (means, miniums, maximums etc.)"
 summary(songs)
 
-# If there are some type of missing worng write like "*", or "?", or"NA", or another type of missings wrong format,
-# summary should not calculate anything in the variable where there are wrong missing format. As we can see all the
-# summary calculates like there are unique missing format. But we need to observe if theere are a numerical missing format,
-# If we order the diferents columns we can see if there are some type of irregular number that could represent a missing.
-# There are any irrugular number, so we can think that there are any other wrong missing format.
+"Things that we can observe:
+  1. Same cuantity of NA's in every attribute, but they are located randomly.
+  2. What is '(Other)' value on key and signature attributes?
+"
 
-### Imputations ###
+songs_long <- songs %>% 
+  pivot_longer(
+    cols = where(is.numeric),
+    names_to = "attributes",
+    values_to = "values"
+  )
+
+ggplot(songs_long, aes(x = values)) +
+  geom_histogram() +
+  facet_wrap(~ attributes, scales = "free") +
+  theme_minimal()
+
+"By analyzing missing values, we can detect potential data errors, such as values that are clearly incorrect. 
+When an error is identified, the corresponding value should be replaced with a missing value. The most common data 
+errors are outliers that represent impossible or unrealistic values. (this should be make in other part)"
+"I want to give alternative ideas to histograms: Box plots, histogram of 2 variables, scatterplots, colored 
+scatterplots (acatterplots with groups), labeled scatterplot (scatterplot with labels) and bubble plot.
+"
+
+### Real start of missings values ###
+
+
 
