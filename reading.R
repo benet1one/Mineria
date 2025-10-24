18af83690abbe98b0cac93afa2b09cfbd3fb02e2
benetsugarboy <benet1one@gmail.com>
2025-10-24 09:47:29 +0200

    new function song_data_frame()
    takes a dataframe and transforms the variables for correct interpretation

--- reading.R
+++ reading.R
@@ -18,25 +18,25 @@ major_relative <- function(key, mode) {
     )
 }
 
+song_data_frame <- function(df) {
+    df <- df |> 
+        as_tibble() |> 
+        mutate(
+            # Minor or Major
+            audio_mode = factor(audio_mode, levels = 0:1, labels = c("Minor", "Major")),
+            key = factor(key, levels = 0:11, labels = KEY_NOTES),
+            time_signature = factor(time_signature, levels = 3:5)
+        )
+    # Values 0 and 1 are not valid time signatures.
+    # Metadata confirms values should range from 3 to 7, but data only contains up to 5.
+    # We convert it into factor because, while the information is technically numerical,
+    # its interpretation is not.
+    
+    # Obvious error, tempo can not be zero
+    df$tempo[df$tempo == 0L] <- NA
+    return(df)
+}
+
 songs <- read.csv("data/train.csv") |> 
     distinct(pick(!ID), .keep_all = TRUE) |> 
-    as_tibble() |> 
-    mutate(
-        # Minor or Major
-        audio_mode = factor(audio_mode, levels = 0:1, labels = c("Minor", "Major")),
-        # Key
-        key = factor(key, levels = 0:11, labels = KEY_NOTES),
-    )
-
-# Obvious error, tempo can not be zero
-songs$tempo[songs$tempo == 0L] <- NA
-# Next lowest value is feasible
-min(songs$tempo, na.rm = TRUE)
-
-# Values 0 and 1 are not valid time signatures.
-# Metadata confirms values should range from 3 to 7, but data only contains up to 5.
-# I convert it into factor because, while the information is technically numerical, 
-# its interpretation is not.
-songs$time_signature <- factor(songs$time_signature, levels = 3:5)
-
-rm(KEY_NOTES)
+    song_data_frame()
