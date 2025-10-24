a9aafa59c54250040e24644a06fc1520638fbf2e
benetsugarboy <benet1one@gmail.com>
2025-10-24 11:06:26 +0200

    improvements on validation plots

--- Bernat/missing_validation.R
+++ Bernat/missing_validation.R
@@ -4,17 +4,31 @@ songs_imp <- readRDS("data/songs_imputed.RDS")
 where_imp <- is.na(songs) |> as_tibble()
 
 bivariate_imputation_plot <- function(x, y) {
-    n_imputed <- where_imp |> 
-        select({{x}}, {{y}}) |> 
-        rowSums() |> 
-        factor(levels = 0:2, labels = c("Original", "One Imputed", "Both Imputed"))
+    imp_cols <- where_imp |> select({{x}}, {{y}})
+    r_sums <- rowSums(imp_cols)
+
+    imp <- character(nrow(imp_cols))
+    imp[r_sums == 0] <- "Original"
+    imp[r_sums == 2] <- "Imputed both"
+    imp[r_sums == 1] <- if_else(
+        unlist(imp_cols[r_sums == 1, 1]),
+        paste("Imputed", names(imp_cols)[1]),
+        paste("Imputed", names(imp_cols)[2])
+    )
     
-    ggplot(songs_imp, aes(x = {{x}}, y = {{y}}, color = n_imputed)) +
+    imp <- factor(imp, levels = c(
+        "Original",
+        paste("Imputed", names(imp_cols)[1]),
+        paste("Imputed", names(imp_cols)[2]),
+        "Imputed both"
+    ))
+    
+    ggplot(songs_imp, aes(x = {{x}}, y = {{y}}, color = imp)) +
         geom_point(alpha = 0.4) +
         theme_minimal() +
         scale_color_manual(
             name = "Imputation", 
-            values = c("gray20", "orange", "red4")
+            values = c("gray20", "royalblue", "orange2", "red4")
         )
 }
 
