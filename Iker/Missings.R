550dbf58eed23906a266623af81124ebcf099622
Torponcito <107923609+Torponcito@users.noreply.github.com>
2025-10-29 14:10:52 +0100

    VIII

--- Iker/Missings.R
+++ Iker/Missings.R
@@ -1,6 +1,8 @@
-
 library(tidyverse)
 library(visdat)
+library(naniar)
+library(ggplot2)
+
 setwd("D:/Iker/VSC-Python/Mineria")
 source("reading.R")
 
@@ -27,6 +29,7 @@ songs %>%
   arrange(desc(N_missings))
 
 # 2. Standardize missing code
+## Pre-analysis to locate rare thinks.
 songs %>%
   summarise(across(where(is.numeric),
                    list(min = ~min(., na.rm = TRUE),
@@ -37,6 +40,37 @@ songs %>%
   mutate(across(c(min, max), as.numeric)) %>%
   arrange(desc(max))
 
+## Solving rare cases.
+songs %>%
+  summarise(across(where(is.numeric),
+                   list(min = ~min(., na.rm = TRUE),
+                        q1  = ~quantile(., 0.25, na.rm = TRUE),
+                        median = ~median(., na.rm = TRUE),
+                        mean = ~mean(., na.rm = TRUE),
+                        q3  = ~quantile(., 0.75, na.rm = TRUE),
+                        max = ~max(., na.rm = TRUE),
+                        sd = ~sd(., na.rm = TRUE)))) %>%
+  pivot_longer(everything(),
+               names_to = c("Variable", ".value"),
+               names_sep = "_") %>%
+  arrange(desc(max))
+
+"Loudness is suspicious, only two values above 0"
+songs |> arrange(-loudness) |> select(loudness, energy)
+plot(energy ~ loudness, data = songs, col = ifelse(songs$loudness > 0, "red", "black"))
+
+"One of them is slightly above zero. Changing its value to zero is convenient later on.
+The other one is a clear univariate outlier. Because it has high energy,
+which is heavily correlated with loudness, we impute it as zero."
+songs$loudness[songs$loudness >= 0] <- (-2e-8)
+
+numeric_data <- songs %>% select(where(is.numeric))
+cor_matrix <- cor(numeric_data, use = "pairwise.complete.obs")
+print(cor_matrix)
+
+plot(audio_valence ~ energy, data = songs)
+
+## Generic transformation of other format missings.
 missing_expressions <- c("?", "NA ", "N/A", "none", "None", ".", "", " ")
 
 songs_original <- songs
@@ -83,143 +117,56 @@ missing_rows <- songs %>%
 missing_rows %>%
   ggplot(aes(x = prop_miss)) +
   geom_histogram(bins = 30) +
-  labs(title = "Distribución de proporción de missing por individuo",
-       x = "Proporción de valores faltantes",
-       y = "Frecuencia")
+  labs(title = "Distribution of proportion missings by instance",
+       x = "Proportion  missing values",
+       y = "Frequency")
 
 # 4. Analyse missing patron
 vis_miss(songs)
 
-library(naniar)
 gg_miss_upset(songs)
 
-library(BaylorEdPsych)
-test_MCAR <- LittleMCAR(df)
+test_MCAR <- mcar_test(songs)
 test_MCAR$p.value
-"The summary says that all the variables of songs data (except ID and song_popularity) have 3956 missing values, and 
-the vis_miss function gives a plot with the missing located, "
-"Things that we can observe:
-  1. Same cuantity of NA's in every attribute, but they are located randomly.
-  2. What is '(Other)' value on key and signature attributes?
-"
 
-songs_long <- songs %>% 
-  pivot_longer(
-    cols = where(is.numeric),
-    names_to = "attributes",
-    values_to = "values"
-  )
+"As p-value > 0.05, we can not reject H_0, so the missings are MCAR."
 
-ggplot(songs_long, aes(x = values)) +
-  geom_histogram() +
-  facet_wrap(~ attributes, scales = "free") +
-  theme_minimal()
-
-"By analyzing missing values, we can detect potential data errors, such as values that are clearly incorrect. 
-When an error is identified, the corresponding value should be replaced with a missing value. The most common data 
-errors are outliers that represent impossible or unrealistic values. (this should be make in other part)"
-"I want to give alternative ideas to histograms: Box plots, histogram of 2 variables, scatterplots, colored 
-scatterplots (acatterplots with groups), labeled scatterplot (scatterplot with labels) and bubble plot.
-"
-
-### Real start of missings values ###
-
-"First of all we want to see the quality of our instances"
-songs_modified <- songs %>% 
-  mutate(missing_quantity = rowSums(is.na(.)))
-
-summary(songs_modified$missing_quantity)
-
-"We can observe that there are 10 missings attributes of a total of 14 attributes in some instaces. We need to 
-study if this instances are a problem by her high missing ratio."
-
-"The reasons why we want to replace the missing values are:
-1. The mining method may have issues handling missing data.
-2. If we want to aggregate the data for other mining processes, the missing values can prevent us from aggregating 
-the data correctly.
-2. If the mining method has an automatic way of handling missing values, it will most likely eliminate the instances 
-with missing data, which can introduce bias. Other methods may perform automatic substitutions, but these replacements 
-might not be appropriate for our specific context or because the mining method doesn’t have access to all the relevant 
-information from other attributes or tables."
-
-"We have five options to handle missing values:
-1. Ignore: Some algorithms are robust to missing values (for example, decision trees).
-2. Eliminate an attribute: When a column has an extremely high proportion of missing values, sometimes it’s better 
-to remove it entirely if it cannot be properly recovered.
-3. Filter out the row: This may introduce bias, since the reasons for missing data are often related to specific 
-cases or patterns in the dataset.
-4. Replace the value: We can replace missing values manually (if there are only a few) or automatically using 
-methods that preserve the mean or variance (globally or within specific groups). A more sophisticated approach 
-is to predict the missing value using other examples — for instance, through regression models or other imputation 
-techniques.
-5. Segment: The tuples are segmented based on the values they have available. Different models are then built for 
-each segment and later combined.
-
-The most common approach is value replacement, but it comes with two main problems:
-First, we lose information, because the imputed value becomes indistinguishable from the original values, and we lose 
-the signal that it was missing.
-Second, we fabricate information, since the imputed value might be inaccurate, introducing potential errors.
-
-A good solution is to create an additional logical (boolean) attribute indicating whether the original value was 
-missing or not. Many data mining methods can take this extra attribute into account to handle missing values more 
-carefully."
-
-# (Iker)
-"
-- Imputation
-- Interpolation (for temporal data), 
-- Model-based imputation (predictive model)(regression, local least square imputation, Support Vector Machine, 
-    decision tree, artificial neural net),  
-- Performing the missing imputation (Maximum Likelihood Estimation),
-- EM algorithms (Expectetion-Maximization algorithms),
-- Multiples imputation,
-- Distance-based methods (k-NN),
-- MICE method,
-- MIMMI method,
-- Other solutions (fuzzy sets, probability distributions or confidence intervals)
-
-
-Of all the posible mathods, the most useful and practical methods are k-NN, MICE, MIMMI and Random Forest Imputation.
-
-"
-
-# (Bernat)
+# Imputation of the missing ----------------------------------------------------
 "We can impute mode from key signature"
 
 mode_prop <- table(songs$audio_mode) |> proportions()
 key_mode_prop <- table(songs$key, songs$audio_mode) |> proportions(margin = 1)
 
 songs <- songs |> mutate(
-    prob_major = if_else(
-        !is.na(audio_mode),
-        as.numeric(audio_mode == "Major"),
-        if_else(
-            !is.na(key),
-            key_mode_prop[key, "Major"],
-            mode_prop["Major"]
-        )
-    ),
-    audio_mode = factor(prob_major > 0.6, labels = c("Minor", "Major"))
+  prob_major = if_else(
+    !is.na(audio_mode),
+    as.numeric(audio_mode == "Major"),
+    if_else(
+      !is.na(key),
+      key_mode_prop[key, "Major"],
+      mode_prop["Major"]
+    )
+  ),
+  audio_mode = factor(prob_major > 0.6, labels = c("Minor", "Major"))
 )
 
 # Mode Proportion before
 mode_prop
 # Mode Proportion now
 table(songs$audio_mode) |> proportions()
+  
+ggplot(songs, aes(x = loudness, y = energy)) +
+  geom_point(alpha = 0.5, color = "blue") +
+  labs(title = "Relación Loudness vs Energy",
+       x = "Loudness",
+       y = "Energy")
 
+lm_model <- lm(energy ~ loudness, data = songs)
+summary(lm_model)
 
-# Found Outliers --------------------------------------------------
-
-# Loudness is suspicious, only two values above 0
-songs |> arrange(-loudness) |> select(loudness, energy)
-plot(energy ~ loudness, data = songs, col = ifelse(songs$loudness > 0, "red", "black"))
-
-# One of them is slightly above zero. Changing its value to zero is convenient later on.
-# The other one is a clear univariate outlier. Because it has high energy,
-# which is heavily correlated with loudness, we impute it as zero.
-songs$loudness[songs$loudness >= 0] <- (-2e-8)
-
-# Mice ------------------------------------------------------------
+# Revisar residuos
+plot(lm_model, which = 1)  # Residuals vs Fitted
+plot(lm_model, which = 2)  # Q-Q plot
 
 # Transform energy and acousticness
 logit <- \(x) log(x / (1 - x))
@@ -259,15 +206,17 @@ predictor_matrix["loudness", "speechiness"] <- 0L
 predictor_matrix[c("loudness", "instrumentalness", "speechiness"), "acousticness"] <- 0L
 
 songs_mice <- mice::mice(
-    data = songs,
-    m = 5,
-    method = methods,
-    predictorMatrix = predictor_matrix,
-    seed = 5151,
+  data = songs,
+  m = 5,
+  method = methods,
+  predictorMatrix = predictor_matrix,
+  seed = 5151,
 )
 
 songs_imputed <- mice::complete(songs_mice, action = songs_mice$m) |> 
-    mutate(loudness = pmin(loudness, 0), acousticness = logit_inv(acousticness)) |> 
-    as_tibble()
+  mutate(loudness = pmin(loudness, 0), acousticness = logit_inv(acousticness)) |> 
+  as_tibble()
 
 saveRDS(songs_imputed, "data/songs_imputed.RDS")
+
+
