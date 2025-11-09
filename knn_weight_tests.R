56a741ae54faf98f72282284ae11690ee23c2c01
benetsugarboy <benet1one@gmail.com>
2025-11-09 16:38:42 +0100

    Perfected KNN
    - Feature Selection and Weighting.
    - Removed oversampling.
    - Changed loss function from RMSE to MAPE.
    - Saved prediction.

--- knn_weight_tests.R
+++ knn_weight_tests.R
@@ -4,29 +4,8 @@ library(ggplot2)
 
 songs <- readRDS("data/songs_imputed.RDS")
 
-distance_daisy <- songs |>
-    slice_sample(n = 5000) |> 
-    select(-ID, -song_popularity, -key, -prob_major) |>
-    cluster::daisy("gower")
-
-distance_mat <- as.matrix(distance_daisy)
-
-k_exploration <- tibble(
-    i1 = sample.int(nrow(distance_mat)),
-    i2 = sample.int(nrow(distance_mat))
-) |> 
-    rowwise() |>
-    mutate(
-        distance = distance_mat[i1, i2],
-        pop_diff = abs(
-            songs$song_popularity[i1] - songs$song_popularity[i2]
-        )
-    ) |>
-    ungroup() |>
-    mutate(distance_cat = cut(distance, breaks = 10)) |>
-    print()
-
-
+# Feature Weighting
+set.seed(123)
 
 dist_contribution <- function(df, i1, i2) {
     lapply(df, function(variable) {
@@ -42,19 +21,24 @@ songs_scaled <- songs |>
     select(-ID, -prob_major) |> 
     mutate(across(where(is.numeric), function(x) scale(x)[, 1]))
 
+# Sample pairs of close observations and calculate individual contributions to the
+# euclidian distance.
+
 nn_idx <- songs_scaled |> 
     select(where(is.numeric), -song_popularity) |> 
     FNN::knn.index()
 
-close_distances <- reshape2::melt(nn_idx, varnames = c("i1", "k"), value.name = "i2") |> 
+close_distances <- nn_idx |> 
+    reshape2::melt(varnames = c("i1", "k"), value.name = "i2") |> 
     as_tibble() |> 
     select(-k) |> 
-    slice_sample(n = 10e3) |> 
+    slice_sample(n = 50e3) |> 
     rowwise() |> 
     mutate(contribution = list(dist_contribution(songs_scaled, i1, i2))) |> 
     tidyr::unnest_wider(contribution) |> 
     select(-i1, -i2)
 
+# Fit (y - y)^2 ~ (x1 - x1)^2 + (x2 - x2)^2 + ...
 
 design_mat <- model.matrix(song_popularity ~ . + 0, data = close_distances)
 head(design_mat)
@@ -64,7 +48,8 @@ while (ncol(design_mat) > 1) {
     coef <- coefficients(fit)
     
     if (min(coef) < 0) {
-        cat("\nEliminated", names(which.min(coef)))
+        # Remove negative coefficients and fit again
+        cat("Eliminated", names(which.min(coef)), "\n")
         design_mat <- design_mat[, -which.min(coef)]
         next
     } else {
@@ -72,5 +57,15 @@ while (ncol(design_mat) > 1) {
     }
 }
 
+last_fit <- lm(song_popularity ~ 0 + (. - instrumentalness - loudness), data = close_distances)
+summary(last_fit)
 
+# To make sure we don't eliminate too much information, we keep variables with p-value < 0.2
+coef <- coefficients(summary(last_fit)) |> 
+    as_tibble(rownames = "variable") |> 
+    rename(weight = Estimate, p_value = "Pr(>|t|)") |> 
+    filter(p_value < 0.2) |> 
+    print()
 
+# Coefficients represent the weight each variable should have in the knn.
+saveRDS(coef, file = "knn_weights.RDS")
